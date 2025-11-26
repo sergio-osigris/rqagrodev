fe943dd064529f5fa9112c7791466b213eb325bc
Sergio <scambeiro@osigris.com>
2025-11-26 11:11:23 +0100

    Bug

--- app/lib/graphs/agent_with_tools/tools/osigris.py
+++ app/lib/graphs/agent_with_tools/tools/osigris.py
@@ -98,6 +98,7 @@ def validar_cultivo(cultivo: str, id_campaña: str, variedad: str) -> str:
                 sigpacs_id = [item["id"] for item in d["sigpac"]]
                 dimension = d["dimension"]
                 datos_cutlvio_variedad.append(f"Nombre:{nombre} Variedad:{variedad} Sigpacs:{sigpacs_id} Dimension: {dimension}")
+            logging.info(f"Existen varios cultivos en la campaña indicada. Elige uno de estos cultivos-variedad: {nombres}. Datos de los cultivos variedad obtenidos: {datos_cutlvio_variedad}.")
             return f"Existen varios cultivos en la campaña indicada. Elige uno de estos cultivos-variedad: {nombres}. Datos de los cultivos variedad obtenidos: {datos_cutlvio_variedad}."   
     else:
         return f"No encuentro ningún cultivo en la campaña indicada"
