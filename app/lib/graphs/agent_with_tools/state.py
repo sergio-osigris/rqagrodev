773ff992f4a1d68bb73d07e473a2962783d5b57a
Sergio <scambeiro@osigris.com>
2025-12-04 16:56:54 +0100

    Añadir validacion campaña con respuesta por whatsapp

--- app/lib/graphs/agent_with_tools/state.py
+++ app/lib/graphs/agent_with_tools/state.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel,Field
-from typing import Annotated, Literal
+from typing import Annotated, Literal, Optional
 from langchain_core.messages import BaseMessage
 import operator
 from typing import List,Dict, Any
@@ -13,17 +13,17 @@ class ChatState(BaseModel):
     name: str = Field(description="Full name of user")
     record_generated: bool = Field(description="Indicates if the current conversation has already been generated a new record.",default=False)
     record: RecordBase = Field(description="")
-    check_errors: list[str] = []
-    check_status: str | None = None
-    check_messages: list[str] = Field(default_factory=list)
+    check_errors: Optional[list[str]] = []
+    check_status: Optional[str | None] = None
+    check_messages: Optional[list[str]] = Field(default_factory=list)
     # osigris_token:
 
     # para campaña
-    campaign_validated: bool | None
-    campaign_id: str | None
-    campaign_options: list[dict] | None  # para el caso de varias
-    campaign_need_choice: bool | None
-    campaign_need_fix: bool | None
+    campaign_validated: Optional[bool] = None
+    campaign_id: Optional[str] = None
+    campaign_options: Optional[List[Dict[str, Any]]] = None
+    campaign_need_choice: bool = False
+    campaign_need_fix: bool = False
 
     class Config:
         arbitrary_types_allowed = True
